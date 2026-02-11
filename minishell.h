cae988139dc6386eddccda71fcb5c5d29c663fa0
MAR-S-81 <mchesnea@student.42angouleme.fr>
2026-02-11 19:51:17 +0100

    add signal c, \, d

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mchesnea <mchesnea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 17:52:08 by mchesnea          #+#    #+#             */
-/*   Updated: 2026/02/10 18:02:01 by mchesnea         ###   ########.fr       */
+/*   Updated: 2026/02/11 19:50:20 by mchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <libft/libft.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+# include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
 
@@ -23,6 +24,33 @@
 # define GREEN "\001\033[1;32m\002"
 # define ORANGE "\001\033[1;33m\002"
 
-char	***parsing(char **argv, char **envp);
+extern int			g_signal_status;
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_HERE_DOC,
+	TOKEN_APPEND
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	t_token			*next;
+}					t_token;
+
+typedef struct s_args
+{
+	char	**args;
+	int		fd_in;
+	int		fd_out;
+	t_token	*next;
+}			t_args;
+
+t_args	**parsing(char **argv, char **envp);
 
 #endif
