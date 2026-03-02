957f14764faa601349f6e2f91b15a8a881a587ae
MAR-S-81 <mchesnea@student.42angouleme.fr>
2026-03-02 16:05:46 +0100

    norm actual folder

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mchesnea <mchesnea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 17:52:08 by mchesnea          #+#    #+#             */
-/*   Updated: 2026/02/27 16:48:59 by mchesnea         ###   ########.fr       */
+/*   Updated: 2026/03/02 16:05:36 by mchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,32 +30,32 @@
 # define GREEN "\001\033[1;32m\002"
 # define ORANGE "\001\033[1;33m\002"
 
-extern int g_signal_status
+extern int			g_signal_status;
 
-// typedef enum e_token_type
-// {
-// 	TOKEN_WORD,
-// 	TOKEN_PIPE,
-// 	TOKEN_REDIR_IN,
-// 	TOKEN_REDIR_OUT,
-// 	TOKEN_HERE_DOC,
-// 	TOKEN_APPEND
-// }					t_token_type;
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_HERE_DOC,
+	TOKEN_APPEND
+}					t_token_type;
 
-// typedef struct s_token
-// {
-// 	char			*value;
-// 	t_token_type	type;
-// 	t_token			*next;
-// }					t_token;
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	t_token			*next;
+}					t_token;
 
-// typedef struct s_args
-// {
-// 	char			**args;
-// 	int				fd_in;
-// 	int				fd_out;
-// 	t_args			*next;
-// }					t_args;
+typedef struct s_args
+{
+	char			**args;
+	int				fd_in;
+	int				fd_out;
+	t_args			*next;
+}					t_args;
 
 typedef struct s_env
 {
@@ -64,13 +64,13 @@ typedef struct s_env
 	struct s_env	*next;
 }					t_env;
 
-// void				quotes_handling(t_token **token, char *arg, int *i, int *j);
-// void				token_typer(t_token **token);
-// t_token				*create_node(t_token **token);
-// void				arger(t_token **token, t_token **token_tmp, char *arg,
-// 						int *i);
-// int					parsing(char *arg, char **envp);
-// void				clean_exit(t_token *tokens);
+void				quotes_handling(t_token **token, char *arg, int *i, int *j);
+void				token_typer(t_token **token);
+t_token				*create_node(t_token **token);
+void				arger(t_token **token, t_token **token_tmp, char *arg,
+						int *i);
+int					parsing(char *arg, char **envp);
+void				clean_exit(t_token *tokens);
 char				*get_args_envp(char *str, t_env *lst);
 void				split_key_values(char **envp, t_env **lst);
 char				**env_list_to_tab(t_env *env);
